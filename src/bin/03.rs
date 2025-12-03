3f62e8db35e0e381799954bdf8479029545665d4
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2025-12-03 20:01:06 +0100

    Day 3

---
+++ src/bin/03.rs
@@ -0,0 +1,39 @@
+use itertools::Itertools;
+
+advent_of_code::solution!(3);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let data = input
+        .lines()
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect_vec());
+    let mut total = 0;
+    for d in data {
+        let a = d[..d.len() - 1].iter().max().unwrap();
+        let (max_index, _) = d.iter().find_position(|&c| c == a).unwrap();
+        let b = d[max_index + 1..].iter().max().unwrap();
+        total += a * 10 + b;
+    }
+
+    Some(total)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(357));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
