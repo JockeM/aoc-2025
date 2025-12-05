a3723173e55f320782a61599f38618b9f22f7998
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2025-12-05 21:19:38 +0100

    Day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -18,7 +18,6 @@ pub fn part_one(input: &str) -> Option<u64> {
         for &(low, high) in ranges.iter() {
             if value >= low && value <= high {
                 sum += 1;
-                println!("{value}");
                 break;
             }
         }
@@ -28,7 +27,31 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let ranges = input.split_once("\n\n")
+        .map(|(ranges, _)| ranges)
+        .unwrap()
+        .lines()
+        .map(|line| {
+            line.split_once("-")
+                .map(|(a, b)| (a.parse::<u64>().unwrap(), b.parse::<u64>().unwrap()))
+                .unwrap()
+        })
+        .sorted_unstable_by(|a, b| a.0.cmp(&b.0))
+        .collect_vec();
+
+    let mut current_high = 0;
+    let mut count = 0;
+    for &(low, high) in &ranges {
+        if low > current_high {
+            count += high - low + 1;
+            current_high = high;
+        } else if high > current_high {
+            count += high - current_high;
+            current_high = high;
+        }
+    }
+
+    Some(count)
 }
 
 #[cfg(test)]
@@ -44,6 +67,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(14));
     }
 }
